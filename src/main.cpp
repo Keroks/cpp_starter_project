4a9f5fc5522449ec6203d3f2653941fda15a4e57
Adam Skorek <adam.keroks@gmail.com>
2020-04-02 14:21:22 +0200

    Minimal version working with vscode

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,16 @@
 #include <iostream>
 
 static constexpr auto USAGE =
-  R"(Naval Fate.
+  R"(cpp_starter_project
 
     Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
+          cpp_starter_project
+          cpp_starter_project (-h | --help)
+          cpp_starter_project --version
     Options:
           -h --help     Show this screen.
           --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
+          --valueWithDefault=<value>  Some Value [default: 0].
 )";
 
 int main(int argc, const char **argv)
@@ -31,7 +26,7 @@ int main(int argc, const char **argv)
   std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
     { std::next(argv), std::next(argv, argc) },
     true,// show help if requested
-    "Naval Fate 2.0");// version string
+    "cpp_starter_project 1.0");// version string
 
   for (auto const &arg : args) {
     std::cout << arg.first << arg.second << std::endl;
